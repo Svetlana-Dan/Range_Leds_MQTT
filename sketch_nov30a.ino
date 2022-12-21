1440611619d46932a8a13f0d682988dd00b0bc35
Svetlana-Dan <76607753+Svetlana-Dan@users.noreply.github.com>
2022-12-21 08:39:53 +0800

    Update sketch_nov30a.ino

--- sketch_nov30a.ino
+++ sketch_nov30a.ino
@@ -11,7 +11,7 @@ void setup() {
   MQTT_init();
   init_sensor();
   // mqtt_cli.publish("esp8266/state", ("hello from" + id()).c_str(), "hello");
-  mqtt_cli.publish((mqtt_client_id +"/state").c_str(), "hello");
+  // mqtt_cli.publish((mqtt_client_id +"/state").c_str(), "hello");
   // mqtt_cli.subscribe(sensor_topic);
 }
 
@@ -19,8 +19,8 @@ void loop() {
   server.handleClient();
   mqtt_cli.loop();
   float range = get_range();
-  mqtt_cli.publish((mqtt_client_id +"/range").c_str(), String(range).c_str());
-  Serial.println(mqtt_client_id +"/range");
+  //mqtt_cli.publish((mqtt_client_id +"/range").c_str(), String(range).c_str());
+  //Serial.println(mqtt_client_id +"/range");
   delay(500);
-  // mqtt_cli.publish("esp8266/state", ("hello from" + id()).c_str(), "hello");
+  // mqtt_cli.publish("esp8266/state", String(range).c_str());
 }
